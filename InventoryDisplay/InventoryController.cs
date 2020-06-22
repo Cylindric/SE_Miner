657983c4d8aa48ebbec73d266606e55ba8638f08
Cylindric <mark@hanfordonline.co.uk>
2020-06-22 23:52:27 +0100

    Inventory

---
+++ InventoryDisplay/InventoryController.cs
@@ -0,0 +1,118 @@
+﻿using Sandbox.ModAPI.Ingame;
+using System.Collections.Generic;
+using System.Linq;
+using VRage;
+
+namespace IngameScript
+{
+    class InventoryController
+    {
+        readonly Program _prog;
+        readonly IMyGridTerminalSystem _grid;
+        List<IMyFunctionalBlock> _containers = new List<IMyFunctionalBlock>();
+
+        public InventoryController(Program program)
+        {
+            _prog = program;
+            _grid = _prog.GridTerminalSystem;
+        }
+
+        /// <summary>
+        /// Finds all containers on the currently-connected grid.
+        /// </summary>
+        public void FindAllContainers()
+        {
+            var all_containers = new List<IMyFunctionalBlock>();
+            _grid.GetBlocksOfType(all_containers);
+            _containers.Clear();
+            foreach (var c in all_containers)
+            {
+                if (c.HasInventory)
+                {
+                    _containers.Add(c);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Calculate the percentage of storage currently in use.
+        /// </summary>
+        /// <returns>Consumed percentage</returns>
+        public float GetUsedStoragePercentage()
+        {
+            MyFixedPoint free_volume = 0;
+            MyFixedPoint total_volume = 0;
+            MyFixedPoint current_volume = 0;
+
+            foreach (var c in _containers)
+            {
+                for (int i = 0; i < c.InventoryCount; i++)
+                {
+                    var inv = c.GetInventory(i);
+                    total_volume += inv.MaxVolume;
+                    current_volume += inv.CurrentVolume;
+                    free_volume += (inv.MaxVolume - inv.CurrentVolume);
+                }
+            }
+            float usedPercentage = ((float)current_volume / (float)total_volume);
+            if (float.IsNaN(usedPercentage))
+            {
+                return 0F;
+            }
+            return usedPercentage;
+        }
+
+        /// <summary>
+        /// Return a collection of every stored item of the specified type and 
+        /// the currently stored amount.
+        /// </summary>
+        /// <param name="typeId">The type of object to include.</param>
+        /// <returns>A collection of types and quantities.</returns>
+        private Dictionary<string, float> GetItemCounts(string typeId)
+        {
+            Dictionary<string, float> itemlist = new Dictionary<string, float>();
+
+            foreach (var c in _containers)
+            {
+                for (int i = 0; i < c.InventoryCount; i++)
+                {
+                    var inv = c.GetInventory(i);
+                    List<VRage.Game.ModAPI.Ingame.MyInventoryItem> items = new List<VRage.Game.ModAPI.Ingame.MyInventoryItem>();
+                    inv.GetItems(items);
+                    foreach (var item in items.Where(x => x.Type.TypeId == typeId))
+                    {
+                        string ore_type = item.Type.SubtypeId;
+
+                        if (!itemlist.ContainsKey(ore_type))
+                        {
+                            itemlist.Add(item.Type.SubtypeId, 0);
+                        }
+                        itemlist[ore_type] += (float)item.Amount;
+                        _prog.Debug($"InvCon: {ore_type}={item.Amount}");
+                    }
+                }
+            }
+            return itemlist;
+        }
+
+        /// <summary>
+        /// Return a collection of every type of ore and the currently stored 
+        /// amount.
+        /// </summary>
+        /// <returns>A collection of ores and quantities.</returns>
+        public Dictionary<string, float> GetOreCounts()
+        {
+            return GetItemCounts("MyObjectBuilder_Ore");
+        }
+
+        /// <summary>
+        /// Return a collection of every type of ingot and the currently stored 
+        /// amount.
+        /// </summary>
+        /// <returns>A collection of ingots and quantities.</returns>
+        public Dictionary<string, float> GetIngotCounts()
+        {
+            return GetItemCounts("MyObjectBuilder_Ingot");
+        }
+    }
+}
---
+++ InventoryDisplay/Program.cs
@@ -0,0 +1,127 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System;
+using VRage.Collections;
+using VRage.Game.Components;
+using VRage.Game.GUI.TextPanel;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRage.Game;
+using VRage;
+using VRageMath;
+
+namespace IngameScript
+{
+    partial class Program : MyGridProgram
+    {
+        List<IMyTextPanel> _oreDisplays = new List<IMyTextPanel>();
+        InventoryController _inventory;
+
+        public Program()
+        {
+            Runtime.UpdateFrequency = UpdateFrequency.Update100;
+
+            Debug("loading...");
+            _inventory = new InventoryController(this);
+            _inventory.FindAllContainers();
+
+            // Find all the panels to use
+            var all_panels = new List<IMyTextPanel>();
+            GridTerminalSystem.GetBlocksOfType(all_panels);
+
+            // Find all the inventory panels
+            _oreDisplays.Clear();
+            _oreDisplays = all_panels.Where(p => p.CustomData.Contains("[OreInventory]")).ToList();
+            foreach (var d in _oreDisplays)
+            {
+                d.ContentType = ContentType.TEXT_AND_IMAGE;
+            }
+        }
+
+        public void Save()
+        {
+        }
+
+        public void Main(string argument, UpdateType updateSource)
+        {
+            if (updateSource == UpdateType.Update100)
+            {
+                UpdateOreDisplays();
+            }
+        }
+
+        public void UpdateOreDisplays()
+        {
+            Debug("updating...");
+            var ore = _inventory.GetOreCounts();
+            string ore_text = string.Empty;
+            foreach (var o in ore.OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+            {
+                string displayValue = $"{o.Value:N0}kg";
+                Debug(ore_text);
+                if (o.Value < 1)
+                {
+                    displayValue = $"{o.Value:N2}kg";
+                }
+
+                ore_text += $"{displayValue,12} {o.Key}\n";
+            }
+
+            var ingots = _inventory.GetIngotCounts();
+            string ingot_text = string.Empty;
+            foreach (var o in ingots.OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+            {
+                string displayValue = $"{o.Value:N0}kg";
+                Debug(displayValue);
+                if (o.Value < 1)
+                {
+                    displayValue = $"{o.Value:N2}kg";
+                }
+
+                ingot_text += $"{displayValue,12} {o.Key}\n";
+            }
+
+            Debug($"updating {_oreDisplays.Count} displays...");
+
+            foreach (var d in _oreDisplays)
+            {
+                d.FontColor = new Color(0.27f, 0f, 0f);
+                d.WriteText(ore_text);
+                d.WriteText("\n\n", true);
+                d.FontColor = new Color(0f, 0.27f, 0f);
+                d.WriteText(ingot_text, true);
+            }
+        }
+
+
+        public void Debug(string msg)
+        {
+            Echo(msg);
+
+            //foreach (var d in _debugDisplays)
+            //{
+            //    lines.Enqueue(msg);
+            //    while (lines.Count > 15)
+            //    {
+            //        lines.Dequeue();
+            //    }
+
+            //    bool first = true;
+            //    foreach (var line in lines.ToArray())
+            //    {
+            //        d.WriteText(line, first == false);
+            //        first = false;
+            //    }
+            //}
+        }
+
+    }
+
+}
